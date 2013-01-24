774e20d07bd2d9e82370d3b6b35b43dc9d919cf3
whalenrp <richard.p.whalen@vanderbilt.edu>
2013-01-24 00:01:26 -0600

    Removed binary files, added base OpenGL system calls

--- bin/GalagaClone-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/richard/Projects/Galaga_Clone/bin/GalagaClone-debug-unaligned.apk : \
-/home/richard/Projects/Galaga_Clone/bin/GalagaClone.ap_ \
-/home/richard/Projects/Galaga_Clone/bin/classes.dex \
--- bin/classes.dex.d
+++
@@ -1,8 +0,0 @@
-/home/richard/Projects/Galaga_Clone/bin/classes.dex : \
-/home/richard/Projects/Galaga_Clone/bin/classes/com/vmat/galagaclone/GalagaMain.class \
-/home/richard/Projects/Galaga_Clone/bin/classes/com/vmat/galagaclone/BuildConfig.class \
-/home/richard/Projects/Galaga_Clone/bin/classes/com/vmat/galagaclone/R$attr.class \
-/home/richard/Projects/Galaga_Clone/bin/classes/com/vmat/galagaclone/R$drawable.class \
-/home/richard/Projects/Galaga_Clone/bin/classes/com/vmat/galagaclone/R$layout.class \
-/home/richard/Projects/Galaga_Clone/bin/classes/com/vmat/galagaclone/R$string.class \
-/home/richard/Projects/Galaga_Clone/bin/classes/com/vmat/galagaclone/R.class \

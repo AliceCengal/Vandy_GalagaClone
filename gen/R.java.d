d536b1a833b6608a95568cf58e9d5de623f640d8
whalenrp <richard.p.whalen@vanderbilt.edu>
2013-01-24 00:05:28 -0600

    Deleted more system-specific files

--- gen/R.java.d
+++
@@ -1,8 +0,0 @@
-/home/richard/Projects/Galaga_Clone/gen/com/vmat/galagaclone/R.java \
- : /home/richard/Projects/Galaga_Clone/res/values/strings.xml \
-/home/richard/Projects/Galaga_Clone/res/layout/main.xml \
-/home/richard/Projects/Galaga_Clone/res/drawable-xhdpi/ic_launcher.png \
-/home/richard/Projects/Galaga_Clone/res/drawable-hdpi/ic_launcher.png \
-/home/richard/Projects/Galaga_Clone/res/drawable-mdpi/ic_launcher.png \
-/home/richard/Projects/Galaga_Clone/res/drawable-ldpi/ic_launcher.png \
-/home/richard/Projects/Galaga_Clone/bin/AndroidManifest.xml \
